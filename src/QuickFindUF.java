2d7bd8ea6a99bc37806305e24ccae83706c5c2a8
jmistry <jayml.mistry@gettyimages.com>
2014-07-07 14:43:44 -0700

    refactor UF into QuickFind variant

--- src/QuickFindUF.java
+++ src/QuickFindUF.java
@@ -1,32 +1,34 @@
 import java.util.Arrays;
 
-public class UnionFind {
+public class QuickFindUF {
     public int[] components;
 
-    public UnionFind(int numberOfComponents) {
+    public QuickFindUF(int numberOfComponents) {
         if (numberOfComponents < 0) throw new IllegalArgumentException();
         components = new int[numberOfComponents];
         for (int i = 0; i < numberOfComponents; i++) components[i] = i;
     }
 
     public void union(int firstComponent, int secondComponent) {
-        components[firstComponent] = components[secondComponent];
+        if (connected (firstComponent, secondComponent)) return;
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] == components[firstComponent]) {
+                components[i] = components[secondComponent];
+            }
+        }
     }
 
     public int find(int componentId) {
-        // finds the root of the component group
-        if (componentId < 0 || componentId >= components.length) throw new IndexOutOfBoundsException();
-        if (componentId != components[componentId]) return find(components[componentId]);
-        else return componentId;
+        return components[componentId];
     }
 
     public boolean connected(int firstComponent, int secondComponent) {
-        return find(firstComponent) == find(secondComponent);
+        return components[firstComponent] == components[secondComponent];
     }
 
     public static void main(String[] args) {
-// TODO: implement this.
-        UnionFind uf = new UnionFind(12);
+        // TODO: implement this.
+        QuickFindUF uf = new QuickFindUF(12);
         uf.union(1, 2);
         uf.union(2, 6);
         uf.union(6, 9);
